83e65f2bb5f4f585cb965b891cd964834346b0bc
JakeBrewskie <jakebruscino@gmail.com>
2025-03-29 12:51:13 -0400

    Emily UI Changes

--- src/pages/game_pages/LoL/Lol_Lan.js
+++ src/pages/game_pages/LoL/Lol_Lan.js
@@ -8,7 +8,7 @@ const Lol_Lan = () => {
             <a href='/lans'>
                 <button type="submit" className='info-button-lans-home'>Back to LANs</button>
             </a>
-            <iframe src="https://www.google.com/maps/d/u/6/embed?mid=1CFHyDVMbDSwBAX2_cz3KqiCm0UY9aQQ&ehbc=2E312F" className='LanMap'></iframe>
+            <iframe src="https://www.google.com/maps/d/u/2/embed?mid=1CFHyDVMbDSwBAX2_cz3KqiCm0UY9aQQ&ehbc=2E312F" className='LanMap'></iframe>
         </div>
     );
 }
