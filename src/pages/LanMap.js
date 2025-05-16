3ae0c6d9c26788bad2996e9a62607d39a4a4ccc0
Matthew <oconnormattc@gmail.com>
2025-05-16 17:37:36 -0400

    Emergency LanMap fix, adding cover and background

--- src/pages/LanMap.js
+++ src/pages/LanMap.js
@@ -2,6 +2,7 @@ import Footer from '../Footer';
 import { Helmet } from "react-helmet";
 import '../Info.css';
 import React from 'react';
+import GrayBox from './hooks/MapCover';
 
 const LanMap = () => {
     const [navHeight, setNavHeight] = React.useState(60); // Default to 60px
@@ -18,7 +19,7 @@ const LanMap = () => {
             <Helmet>
               <meta name="description" content="Find LANs near you. We have every LAN event from across the globe consolidated on our LAN map." />
             </Helmet>
-        <div style={{ paddingTop: `${navHeight}px`, overflowX: 'hidden' }}>
+        <div style={{ paddingTop: `${navHeight}px`, overflowX: 'hidden', backgroundColor: 'rgb(49, 49, 49)' }}>
             <div>
                 <a href='/lans'>
                     <button type="submit" className='info-button-lans'>Back to LANs</button>
@@ -28,6 +29,8 @@ const LanMap = () => {
                 </a> */}
             </div>
             <iframe src="https://www.google.com/maps/d/u/2/embed?mid=1QrCfeXFSyZJMXLzUeAiIWy0hcWgaJVg&ehbc=2E312F" className='full-map'></iframe>
+            <GrayBox />
+            
             <Footer />
         </div>
                 </>
