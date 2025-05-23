e579c48bb6632a19aa3864f828f6b90392f5a660
Matthew <oconnormattc@gmail.com>
2025-05-23 18:36:51 -0400

    Fixing stripe integration issue

--- src/App.js
+++ src/App.js
@@ -2975,7 +2975,7 @@ break;
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch("/create-payment-intent", {
+    fetch("https://website-backend-5m32.onrender.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ items: [{ id: "xl-tshirt", amount: 1000 }] }),
