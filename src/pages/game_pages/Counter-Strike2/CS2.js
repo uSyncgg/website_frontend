9477021417f371a3bfd98864cfcd8ea052c64ee6
tonegmz <tonegmzbusiness@gmail.com>
2025-07-25 00:38:51 -0400

    Update CS2.js
    Adding <Helmet> for improved SEO searchability, ensuring searches for CS2 leagues, Counter-Strike-2 tournaments, and other keywords link to us

--- src/pages/game_pages/Counter-Strike2/CS2.js
+++ src/pages/game_pages/Counter-Strike2/CS2.js
@@ -5,73 +5,65 @@ import useNavHeight from '../../hooks/NavbarHeight';
 
 const CS2 = () => {
   useNavHeight();
-    return (
-      <>
-          <Helmet>
-            <meta name="description" content="Counter-Strike 2 leagues, LANs, wagers, head-to-head, and tournaments all in once place. Try the true competitive experience today." />
-          </Helmet>
+
+  return (
+    <>
+      <Helmet>
+        <title>Counter-Strike 2 | Leagues, Wagers & Tournaments | uSync</title>
+        <meta name="description" content="Find Counter-Strike 2 leagues, head-to-heads, wagers, and tournaments all in one place. The competitive CS2 experience starts here." />
+        <meta property="og:title" content="Play Counter-Strike 2 Competitively | uSync" />
+        <meta property="og:description" content="Join CS2 leagues, tournaments, and head-to-head wagers. Your next competitive CS2 match starts on uSync." />
+        <meta property="og:image" content="https://i.imgur.com/A0AhUZp.jpeg" />
+        <meta property="og:url" content="https://usync.gg/games/CS2" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Competitive CS2 Leagues & Wagers | uSync" />
+        <meta name="twitter:description" content="Browse CS2 events including leagues, wagers, and tournaments. Get verified action on uSync." />
+        <meta name="twitter:image" content="https://i.imgur.com/A0AhUZp.jpeg" />
+      </Helmet>
+
       <div className="total-container">
         <div className='Header-Img-l cs2-general-header-img' />
 
         <div className="Background-game">
           <div className="container">
             <div className="row justify-content-center games-row-1">
-              {" "}
-              {/* add justify-content-center class to center the columns */}
               <div className="col-lg-4 space-on-bottom" id="bubblel">
-              <a href="/Comingsoon">
-              <img
-                    src="https://i.imgur.com/ciqy3Bd.png"
-                    alt="CS2 LANs"
-                    className="blockimg"
-                  />{" "}
+                <a href="/Comingsoon">
+                  <img src="https://i.imgur.com/ciqy3Bd.png" alt="CS2 LANs" className="blockimg" />
                 </a>
               </div>
               <div className="col-lg-4 space-on-bottom" id="bubble">
-              <a href="/Comingsoon">
-              <img
-                    src="https://i.imgur.com/ugKIhLq.png"
-                    alt="CS2 Tournaments"
-                    className="blockimg"
-                  />
+                <a href="/Comingsoon">
+                  <img src="https://i.imgur.com/ugKIhLq.png" alt="CS2 Tournaments" className="blockimg" />
                 </a>
               </div>
               <div className="col-lg-4 space-on-bottom" id="bubbler">
                 <a href="/games/CS2/leagues">
-                  <img
-                    src="https://i.imgur.com/gE6ZfqX.png"
-                    alt="CS2 Leagues"
-                    className="blockimg"
-                  />
+                  <img src="https://i.imgur.com/gE6ZfqX.png" alt="CS2 Leagues" className="blockimg" />
                 </a>
               </div>
             </div>
             <div className="row justify-content-center games-row-2">
               <div className="col-lg-4 space-on-bottom" id="bubblelb">
                 <a href="/games/CS2/wagers">
-                  <img
-                    src="https://i.imgur.com/SzMtvPy.png"
-                    alt="CS2 Wagers"
-                    className="blockimg"
-                  />
+                  <img src="https://i.imgur.com/SzMtvPy.png" alt="CS2 Wagers" className="blockimg" />
                 </a>
               </div>
               <div className="col-lg-4" id="bubblerb">
                 <a href="/games/CS2/head-to-head">
-                  <img
-                    src="https://i.imgur.com/b9B93ig.png"
-                    alt="CS2 head-to-head"
-                    className="blockimg"
-                  />
+                  <img src="https://i.imgur.com/b9B93ig.png" alt="CS2 Head-to-Head" className="blockimg" />
                 </a>
               </div>
             </div>
           </div>
         </div>
+
         <Footer />
       </div>
-      </>
-    );
+    </>
+  );
 }
 
 export default CS2;
