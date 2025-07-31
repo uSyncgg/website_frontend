46dbefc517d4c48c015217adf1a3513eb02c7eb9
tonegmz <tonegmzbusiness@gmail.com>
2025-07-31 14:10:00 -0400

    Update Corporate_CS2.js
    Added canonical + extended meta information for SEO

--- src/pages/game_pages/Counter-Strike2/Leagues/Corporate_CS2.js
+++ src/pages/game_pages/Counter-Strike2/Leagues/Corporate_CS2.js
@@ -15,82 +15,98 @@ const Corporate_CS2 = () => {
   useNavHeight();
   return (
     <>
-        <Helmet>
-          <meta name="description" content="Corporate CS2 league is THE corporate America league. Play with your co-workers against other companies in the United States." />
-        </Helmet>
-        <div className='total-container'>
-        <div className="Header-Img-l not-verified-banner-img-lans"> 
-        <div className="games-leagues-header-title">
-          <h1 id="league-title">Corporate CS2 League</h1>
-          <img src="https://i.imgur.com/eNhKhTI.png" alt="underline" className='Underline-h' />
-        </div>
-      </div>
-      <div className="background-mid">
-        <div className="Background-mid-top">
-          {/* This will be the same for every single file just switch the checkmark to an x if it is not verified 
-                along with changing the uSync Verified to the Not verified image and change the alt to Not Verififed
-                
-                Add id="checkmark-title-link" className='mx-auto my-2' as attributes to the .Checkmark-title a tag
-                Add id="checkmark-link" to the .Checkmark a tag
-                Add id='join-button-link' to the .join-button a tag*/}
-          <a
-            href="/more/verification"
-            id="checkmark-title-link"
-            className="mx-auto my-2"
-          >
-            <img
-              src="https://i.imgur.com/HaRfFh9.png"
-              className="Checkmark-title"
-            />
-          </a>
-          <a href="/more/verification" id="checkmark-link">
-            <img src="https://i.imgur.com/0MPIBOK.png" className="Checkmark" />
-          </a>
-          <a
-            href="https://cea.gg/"
-            target="_blank"
-            rel="noopener noreferrer"
-            id="join-button-link"
-          >
-            <button className="join-button">Join Now</button>
-          </a>
-        </div>
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4 league-info">
-              <h2 className="info-title">Date:</h2>
-              <p>Annual Spring and Fall Seasons</p>
-            </div>
-            <div class="col-md-4 league-info">
-              <h2 className="info-title">Details:</h2>
-              <p>$2,000 in charity donations, no prize pool (Estimated)</p>
-              <p>All matches are played each Saturday</p>
-              <p>~8 week long regular season with playoffs</p>
-              <p>Competitive</p>
-              <h2 className="info-title3">Region/Type:</h2>
-              <p>5v5 - NA</p>
-            </div>
-            <div class="col-md-4 league-info">
-            <h2 className="info-title leagues-entry-fee">Entry Fee:</h2>
-              <p>$200 per Team (Estimated)</p>
-            </div>
+      <Helmet>
+        <title>Corporate CS2 League | uSync</title>
+        <meta name="description" content="Corporate CS2 league is THE corporate America league. Play with your co-workers against other companies in the United States." />
+        
+        {/* SEO Best Practice: Added a self-referencing canonical tag. */}
+        <link rel="canonical" href="https://usync.gg/games/CS2/leagues/corporate" />
+
+      </Helmet>
+      <div className='total-container'>
+        <div className="Header-Img-l not-verified-banner-img-lans">
+          <div className="games-leagues-header-title">
+            <h1 id="league-title">Corporate CS2 League</h1>
+            {/* SEO Optimization: alt text is empty as this image is purely decorative. */}
+            <img src="https://i.imgur.com/eNhKhTI.png" alt="" className='Underline-h' />
           </div>
         </div>
-        <div className="league-back-button-container">
-          <a href="/games/CS2/leagues" id='league-back-button-a'>
-            <button className="back-button">Back</button>
-          </a>
+        <div className="background-mid">
+          <div className="Background-mid-top">
+            {/* This will be the same for every single file just switch the checkmark to an x if it is not verified 
+                  along with changing the uSync Verified to the Not verified image and change the alt to Not Verififed
+                 
+                  Add id="checkmark-title-link" className='mx-auto my-2' as attributes to the .Checkmark-title a tag
+                  Add id="checkmark-link" to the .Checkmark a tag
+                  Add id='join-button-link' to the .join-button a tag*/}
+            <a
+              href="/more/verification"
+              id="checkmark-title-link"
+              className="mx-auto my-2"
+            >
+              {/* SEO Optimization: Added descriptive alt text. */}
+              <img
+                src="https://i.imgur.com/HaRfFh9.png"
+                className="Checkmark-title"
+                alt="Not a uSync Verified Event"
+              />
+            </a>
+            <a href="/more/verification" id="checkmark-link">
+              {/* SEO Optimization: Added descriptive alt text. */}
+              <img
+                src="https://i.imgur.com/0MPIBOK.png"
+                className="Checkmark"
+                alt="Not Verified Icon"
+              />
+            </a>
+            {/* SEO & Accessibility Optimization: Replaced the invalid button inside a link with a single link styled as a button. */}
+            <a
+              href="https://cea.gg/"
+              target="_blank"
+              rel="noopener noreferrer"
+              id="join-button-link"
+              className="join-button"
+            >
+              Join Now
+            </a>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4 league-info">
+                <h2 className="info-title">Date:</h2>
+                <p>Annual Spring and Fall Seasons</p>
+              </div>
+              <div class="col-md-4 league-info">
+                <h2 className="info-title">Details:</h2>
+                <p>$2,000 in charity donations, no prize pool (Estimated)</p>
+                <p>All matches are played each Saturday</p>
+                <p>~8 week long regular season with playoffs</p>
+                <p>Competitive</p>
+                <h2 className="info-title3">Region/Type:</h2>
+                <p>5v5 - NA</p>
+              </div>
+              <div class="col-md-4 league-info">
+                <h2 className="info-title leagues-entry-fee">Entry Fee:</h2>
+                <p>$200 per Team (Estimated)</p>
+              </div>
+            </div>
+          </div>
+          <div className="league-back-button-container">
+             {/* SEO & Accessibility Optimization: Applied the same fix here for the "Back" button. */}
+            <a href="/games/CS2/leagues" id='league-back-button-a' className="back-button">
+              Back
+            </a>
+          </div>
+          <div className="become-verified">
+            <h6>
+              Become <a href="/more/verification">Verified</a>
+            </h6>
+          </div>
         </div>
-        <div className="become-verified">
-          <h6>
-            Become <a href="/more/verification">Verified</a>
-          </h6>
-        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
     </>
-    );
+  );
 }
 
 // Must export default filename otherwise nothing will show up on the web page!
